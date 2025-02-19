85618d92fcc65000591d21166799312fa38e8bfb
Praharshini Potlpalli <praharshininaidu41@gmail.com>
2025-02-19 20:35:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.pageYOffset > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+};
